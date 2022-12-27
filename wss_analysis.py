c9510657592ebcaab92e2d866dffff99a92d7fe4
fcattafesta <filippo.cattafesta99@gmail.com>
2022-12-27 12:04:54 +0100

    WSS analysis progresses

--- wss_analysis.py
+++ wss_analysis.py
@@ -3,39 +3,36 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import h5py
-from scipy.interpolate import UnivariateSpline
 
 from path import data_path, fig_path
-from acf_analysis import t, acf_norm
+from mean import columns
 
-# Since ACF is oscillating, we clearly see that this stochastic process is not stationary.
-# Although, we can measure (by eye!) the period of the oscillation to test the WSS
-# hypothesis
+f = h5py.File(os.path.join(data_path, "post_data.hdf5"), "r")
+df = pd.DataFrame(f.get("H-H1_GWOSC_4KHZ_R1-1126257415-4096"), columns=columns)
+f.close()
 
-dt = 0.15 # seconds
+def mask(arr):
+    m = np.zeros_like(arr, dtype=bool)
+    for i, elm in enumerate(arr):
+        if i % 10000 == 0:
+            m[i] = 1
+    return m
 
-max_i = int(t[-1] / dt)
-i = np.arange(max_i)
-tt_i = i * dt
+y_norm = df["y_norm"].values
+y_var = np.var(y_norm)
+m = mask(y_norm)
+y_norm = y_norm[:1000]
+n = len(y_norm)
+r = np.ones((n, n))
 
-del i
-
-# In order to evaluate ACF in arbitrary points, we build the ACF spline and evaluate it 
-# in the dt multiple points
-
-acf_spline = UnivariateSpline(t, acf_norm, s=0)
-del t
-acf_wss = acf_spline(tt_i)
-del acf_spline, acf_norm
-
-# In fact, if the process is WSS, we should see that ACF values do not change with
-# time distance
+for i in range(n):
+    for j in range(n):
+        r[i, j] = np.mean(y_norm[i] * y_norm[j]) / y_var
 
 fig, ax = plt.subplots()
-ax.set_ylabel("$ACF(i \cdot \Delta t)$")
-ax.set_xlabel("t [s]")
-ax.grid(True, ls="--", alpha=0.5)
-ax.minorticks_on()
-ax.tick_params(direction="in", which="both")
-ax.plot(tt_i[:1000], acf_wss[:1000], color="black", linewidth=0.5)
-fig.savefig(os.path.join(fig_path, "acf_wss_zoom.pdf"), format="pdf")
+c = ax.imshow(r)
+fig.colorbar(c)
+fig.savefig(os.path.join(fig_path, "correlation_matrix.pdf"), format="pdf")
+
+
+
