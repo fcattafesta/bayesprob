f05bad282b771326ec04c2c12722be0b59bc8e1f
fcattafesta <filippo.cattafesta99@gmail.com>
2022-12-28 08:48:39 +0100

    RFFT analysis fixed

--- psd.py
+++ psd.py
@@ -3,30 +3,34 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import h5py
-from scipy.fft import rfft, fftfreq
-
+from scipy.fft import rfft, rfftfreq
 from utils.dataset import data_path, fig_path, get_df
+from utils.masks import alternate_mask
 
 # Reading dataset
 
 df = get_df()
 
 # In order to compute likelihood function, we need Power Spectral Density, 
-# which is the Fourier transform of ACF (Wiener-Kinchin theorem)
+# which is the Fourier transform of ACF (Wiener-Kinchin theorem). 
+# To perform the calculations we use RFFT, which discards negative frequences
+# for real input
 
 acf = df["acf"].values
 psd = rfft(acf)
 
+# Computing sample spacing and the corresponding sample frequences
+
 t = df["t"].values
 dt = t[1]-t[0]
-n = len(psd)
-f = fftfreq(n, d=dt)
+n = acf.size
+f = rfftfreq(n, d=dt)
 
-# Plot 
+# Plot
 
 fig, ax = plt.subplots()
 ax.set_ylabel("PSD")
-ax.set_xlabel("")
+ax.set_xlabel("Hz")
 ax.grid(True, ls="--", alpha=0.5)
 ax.minorticks_on()
 ax.tick_params(direction="in", which="both")
